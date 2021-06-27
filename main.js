71f8a4fc88d81f4afaba7ee002e5f72f2d98494e
Srishti13-2008 <75202666+Srishti13-2008@users.noreply.github.com>
2021-06-27 15:44:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function setup(){
+    canvas=createCanvas(350,350);
+    canvas.center();
+    canvas.position(300,200);
+    video=createCapture(VIDEO);
+    video.hide();
+    classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/fuUf7LF3W/model.json',modelloaded)
+}
+function modelloaded(){
+    console.log('modelloaded')
+}
+function draw(){
+image(video,0,0,300,300)
+classifier.classify(video,gotresult)
+}
+function gotresult(error,results){
+if(error){
+    console.error(error)
+}else{
+    console.log(results)
+    document.getElementById("result_object_name").innerHTML=results[0].label
+    document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(2)
+
+}
+}
